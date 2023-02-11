b600ebc06e72855d9f1fb0d2ae90764b94fc80f2
ArturoGarGarcia <imedite.chronum@gmail.com>
2023-02-11 16:29:43 +0100

    A

--- src/Reigns/clases/Medico.java
+++ src/Reigns/clases/Medico.java
@@ -1,6 +1,31 @@
 
 package Reigns.clases;
 
-public class Medico {
+import java.awt.Color;
+import java.awt.Graphics;
+
+public class Medico extends Personaje{
+    public Medico() {
+        super("El Médico", 0,
+                new String[] {},
+                new String[] {},
+                new String[] {},
+                new String[] {},
+                new String[] {},
+                
+                new String[] {},
+                new String[] {},
+                new String[] {},
+                new String[] {},
+                new String[] {}
+        );
+    }
     
+    public void dibujar(Graphics G){
+        int fX = 2;
+        int fY = 2;
+        int pX = 340;
+        int pY = 100;
+        dibujarCara(G,new Color(255, 219, 172),new Color(0,0,0),fX,fY,pX,pY);
+    }
 }
