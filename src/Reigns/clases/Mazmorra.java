c87579550d8b5d7873f1b9b1d45721e05d3ed861
ArturoGarGarcia <imedite.chronum@gmail.com>
2023-02-13 12:52:48 +0100

    Aa

---
+++ src/Reigns/clases/Mazmorra.java
@@ -0,0 +1,33 @@
+
+package Reigns.clases;
+
+public class Mazmorra {
+    private int posX;
+    private int posY;
+    private boolean mercurio;
+    private boolean venus;
+    private boolean marte;
+    private boolean jupiter;
+    private boolean saturno;
+    private boolean urano;
+    private boolean neptuno;
+    private boolean luna;
+    private boolean sol;
+    
+    public Mazmorra(){
+        posX = 1;
+        posY = 3;
+        mercurio = false;
+        venus = false;
+        marte = false;
+        jupiter = false;
+        saturno = false;
+        urano = false;
+        neptuno = false;
+        luna = false;
+        sol = false;
+    }
+    
+    
+    
+}
