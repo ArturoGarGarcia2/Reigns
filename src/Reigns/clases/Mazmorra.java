5061ad61629090a55dccff5fa839bcdefeb7e44d
ArturoGarGarcia <imedite.chronum@gmail.com>
2023-02-14 10:14:39 +0100

    Aa

--- src/Reigns/clases/Mazmorra.java
+++ src/Reigns/clases/Mazmorra.java
@@ -4,37 +4,36 @@ package Reigns.clases;
 public class Mazmorra {
     private int[] pos;
     private boolean mercurio;
-    private int[] posMercurio = {2,6};
+    private int[] posMercurio = {1,5};
     
     private boolean venus;
-    private int[] posVenus = {2,4};
+    private int[] posVenus = {1,3};
     
     private boolean marte;
-    private int[] posMarte = {2,2};
+    private int[] posMarte = {1,1};
     
     private boolean jupiter;
-    private int[] posJupiter = {4,6};
+    private int[] posJupiter = {3,5};
     
     private boolean saturno;
-    private int[] posSaturno = {4,4};
+    private int[] posSaturno = {3,3};
     
     private boolean urano;
-    private int[] posUrano = {4,2};
+    private int[] posUrano = {3,1};
     
     private boolean neptuno;
-    private int[] posNeptuno = {6,6};
+    private int[] posNeptuno = {5,5};
     
     private boolean luna;
-    private int[] posLuna = {6,4};
+    private int[] posLuna = {5,3};
     
     private boolean sol;
-    private int[] posSol = {6,2};
+    private int[] posSol = {5,1};
     
     
     public Mazmorra(){
-        int[] a = {1,4};
-        int[] b = {6,2};
-        pos = b;
+        int[] a = {0,3};
+        pos = a;
         mercurio = false;
         venus = false;
         marte = false;
@@ -88,16 +87,39 @@ public class Mazmorra {
         if(pos[0]==posSol[0]&&pos[1]==posSol[1]){
             sol = true;
         }
+        if(pos[0]<0){
+            pos[0]++;
+        }
+        if(pos[0]>6){
+            pos[0]--;
+        }
+        if(pos[1]<0){
+            pos[1]++;
+        }
+        if(pos[1]>6){
+            pos[1]--;
+        }
     }
     
     public void ToString(){
-        for(int i = 0 ; i<7 ; i++){
+        System.out.print("("+pos[0]+","+pos[1]+")");
+        System.out.print("("+posMercurio[0]+","+posMercurio[1]+")");
+        System.out.print("("+posVenus[0]+","+posVenus[1]+")");
+        System.out.print("("+posMarte[0]+","+posMarte[1]+")");
+        System.out.print("("+posJupiter[0]+","+posJupiter[1]+")");
+        System.out.print("("+posSaturno[0]+","+posSaturno[1]+")");
+        System.out.print("("+posUrano[0]+","+posUrano[1]+")");
+        System.out.print("("+posNeptuno[0]+","+posNeptuno[1]+")");
+        System.out.print("("+posLuna[0]+","+posLuna[1]+")");
+        System.out.print("("+posSol[0]+","+posSol[1]+")");
+        System.out.println("");
+        for(int i = 6 ; i>-1 ; i--){
             for(int j = 0 ; j<7 ; j++){
                 System.out.print("##### ");
             }
             System.out.println("");
             for(int j = 0 ; j<7 ; j++){
-                int[] a = {i+1,j+1};
+                int[] a = {j,i};
                 String L = a[0]==posMercurio[0]&&a[1]==posMercurio[1]&&mercurio? "V" :
                            a[0]==posVenus[0]&&a[1]==posVenus[1]&&venus? "m" :
                            a[0]==posMarte[0]&&a[1]==posMarte[1]&&marte? "M" :
@@ -106,7 +128,8 @@ public class Mazmorra {
                            a[0]==posUrano[0]&&a[1]==posUrano[1]&&urano? "U" :
                            a[0]==posNeptuno[0]&&a[1]==posNeptuno[1]&&neptuno? "N" :
                            a[0]==posLuna[0]&&a[1]==posLuna[1]&&luna? "L" :
-                           a[0]==posSol[0]&&a[1]==posSol[1]&&sol? "S" : "?";
+                           a[0]==posSol[0]&&a[1]==posSol[1]&&sol? "S" :
+                           a[0]==pos[0]&&a[1]==pos[1]? "R" : "á";
                            
                 System.out.print("# "+L+" # ");
             }
@@ -115,7 +138,7 @@ public class Mazmorra {
                 System.out.print("##### ");
             }
             System.out.println("");
-            System.out.println("");
         }
     }
+    
 }
--- src/Reigns/juego/Pruebas2.java
+++ src/Reigns/juego/Pruebas2.java
@@ -1,12 +1,35 @@
-
 package Reigns.juego;
 
 import Reigns.clases.Mazmorra;
+import Reigns.clases.Rey;
+import java.util.Scanner;
 
 public class Pruebas2 {
+
     public static void main(String[] args) {
         Mazmorra m = new Mazmorra();
-        m.detectarPos();
-        m.ToString();
+        Rey R = new Rey();
+        while (true) {
+            R.alv();
+            m.ToString();
+            boolean a = true;
+            while (a) {
+                int res = new Scanner(System.in).nextInt();
+                if (res == 1) {
+                    m.mover(0, 1);
+                    a = false;
+                } else if (res == 2) {
+                    m.mover(1, 0);
+                    a = false;
+                } else if (res == 3) {
+                    m.mover(0, -1);
+                    a = false;
+                } else if (res == 4) {
+                    m.mover(-1, 0);
+                    a = false;
+                }
+            }
+            m.detectarPos();
+        }
     }
 }
